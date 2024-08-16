a9b1c9310722d290f8a1dc29feed53425a269e85
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2024-08-16 15:00:48 +0200

    register page

---
+++ src/pages/register/index.page.tsx
@@ -0,0 +1,31 @@
+import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
+import { Container, Form, Header } from './styles'
+import { ArrowRight } from 'phosphor-react'
+
+export default function Register() {
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Bem-vindo ao Ignite Call!</Heading>
+        <Text>
+          Precisamos de algumas informações para criar o seu prefil! Ah, você
+          pode editar essas informações depois.
+        </Text>
+        <MultiStep size={4} currentStep={1} />
+      </Header>
+      <Form as="form">
+        <label>
+          <Text size="sm">Nome de usuario</Text>
+          <TextInput prefix="ignite.com/" placeholder="seu-usuario" />
+          <Text size="sm">Nome completo</Text>
+          <TextInput placeholder="Seu nome" />
+        </label>
+
+        <Button type="submit">
+          Proximo passo
+          <ArrowRight />
+        </Button>
+      </Form>
+    </Container>
+  )
+}
