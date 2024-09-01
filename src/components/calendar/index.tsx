00d9784a89fad1da9613424f6a0907964176bed0
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2024-09-01 19:40:20 +0200

    calendar creation first step

---
+++ src/components/calendar/index.tsx
@@ -0,0 +1,60 @@
+import { CaretLeft, CaretRight } from 'phosphor-react'
+import {
+  CalendarActions,
+  CalendarBody,
+  CalendarContainer,
+  CalendarDay,
+  CalendarHeader,
+  CalendarTitle,
+} from './styles'
+import { getWeekDay } from '../../utils/get-week-day'
+
+export function Calendar() {
+  const shortWeekDays = getWeekDay({ short: true })
+
+  return (
+    <CalendarContainer>
+      <CalendarHeader>
+        <CalendarTitle>
+          Setembro <span>2024</span>
+        </CalendarTitle>
+
+        <CalendarActions>
+          <button>
+            <CaretLeft />
+          </button>
+          <button>
+            <CaretRight />
+          </button>
+        </CalendarActions>
+      </CalendarHeader>
+
+      <CalendarBody>
+        <thead>
+          <tr>
+            {shortWeekDays.map((weekday) => (
+              <th key={weekday}>{weekday}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>2</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>3</CalendarDay>
+            </td>
+          </tr>
+        </tbody>
+      </CalendarBody>
+    </CalendarContainer>
+  )
+}
--- src/utils/get-week-day.ts
+++ src/utils/get-week-day.ts
@@ -1,9 +1,17 @@
-export function getWeekDay() {
+interface GetWeekDaysParams {
+  short?: boolean
+}
+
+export function getWeekDay({ short = false }: GetWeekDaysParams) {
   const formatter = new Intl.DateTimeFormat('pt-BR', { weekday: 'long' })
 
   return Array.from(Array(7).keys())
     .map((day) => formatter.format(new Date(Date.UTC(2021, 5, day))))
     .map((weekday) => {
+      if (short) {
+        return weekday.substring(0, 3).toLocaleUpperCase().concat('.')
+      }
+
       return weekday.substring(0, 1).toUpperCase().concat(weekday.substring(1))
     })
 }
