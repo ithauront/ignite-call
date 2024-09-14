2c493eed9feeb810f3f8dd9868cc710ba9647fb2
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2024-09-14 11:45:47 +0200

    disabled days

---
+++ src/pages/api/users/[username]/blocked-dates.api.ts
@@ -0,0 +1,43 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from '../../../../lib/prisma'
+// import dayjs from 'dayjs'
+
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'GET') {
+    return res.status(405).end()
+  }
+
+  const username = String(req.query.username)
+  const { year, month } = req.query
+
+  if (!year || !month) {
+    return res.status(400).json({ message: 'Year or month not specified.' })
+  }
+
+  const user = await prisma.user.findUnique({
+    where: { userName: username },
+  })
+  if (!user) {
+    return res.status(404).json({ message: 'user not found' })
+  }
+
+  const availableWeekDays = await prisma.userTimeInterval.findMany({
+    select: {
+      week_day: true,
+    },
+    where: {
+      user_id: user.id,
+    },
+  })
+
+  const blockedWeekDays = [0, 1, 2, 3, 4, 5, 6].filter((weekDay) => {
+    return !availableWeekDays.some(
+      (availableWeekDays) => availableWeekDays.week_day === weekDay,
+    )
+  })
+
+  return res.json({ blockedWeekDays })
+}
