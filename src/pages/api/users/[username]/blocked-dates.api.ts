e5f3559156db2f1c75c75bf490a1e06b7f61e4f4
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2024-09-15 13:09:53 +0200

    fim da query raw

--- src/pages/api/users/[username]/blocked-dates.api.ts
+++ src/pages/api/users/[username]/blocked-dates.api.ts
@@ -1,51 +1,64 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { prisma } from '../../../../lib/prisma'
-// import dayjs from 'dayjs'
 
 export default async function handle(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  if (req.method !== 'GET') {
-    return res.status(405).end()
+  try {
+    if (req.method !== 'GET') {
+      return res.status(405).end()
+    }
+
+    const username = String(req.query.username)
+    const { year, month } = req.query
+
+    if (!year || !month) {
+      return res.status(400).json({ message: 'Year or month not specified.' })
+    }
+
+    const user = await prisma.user.findUnique({
+      where: { userName: username },
+    })
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' })
+    }
+
+    const availableWeekDays = await prisma.userTimeInterval.findMany({
+      select: {
+        week_day: true,
+      },
+      where: {
+        user_id: user.id,
+      },
+    })
+
+    const blockedWeekDays = [0, 1, 2, 3, 4, 5, 6].filter((weekDay) => {
+      return !availableWeekDays.some(
+        (availableWeekDays) => availableWeekDays.week_day === weekDay,
+      )
+    })
+
+    const blockedDatesRaw: Array<{ date: number }> = await prisma.$queryRaw`
+      SELECT
+        EXTRACT(DAY FROM S.date) AS date,
+        CAST(COUNT(S.date) AS UNSIGNED) AS amount,  
+        ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60) AS size
+      FROM schedulings S
+      LEFT JOIN user_time_intervals UTI
+        ON UTI.week_day = WEEKDAY(DATE_ADD(S.date, INTERVAL 1 DAY))
+      WHERE S.user_id = ${user.id}
+        AND DATE_FORMAT(S.date, "%Y-%m") = ${`${year}-${month}`}
+      GROUP BY EXTRACT(DAY FROM S.date),
+        ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60)
+      HAVING amount >= size
+    `
+
+    const blockedDates = blockedDatesRaw.map((item) => Number(item.date))
+
+    return res.json({ blockedWeekDays, blockedDates })
+  } catch (error) {
+    console.error('API Error: ', error)
+    return res.status(500).json({ message: 'Internal server error' })
   }
-
-  const username = String(req.query.username)
-  const { year, month } = req.query
-
-  if (!year || !month) {
-    return res.status(400).json({ message: 'Year or month not specified.' })
-  }
-
-  const user = await prisma.user.findUnique({
-    where: { userName: username },
-  })
-  if (!user) {
-    return res.status(404).json({ message: 'user not found' })
-  }
-
-  const availableWeekDays = await prisma.userTimeInterval.findMany({
-    select: {
-      week_day: true,
-    },
-    where: {
-      user_id: user.id,
-    },
-  })
-
-  const blockedWeekDays = [0, 1, 2, 3, 4, 5, 6].filter((weekDay) => {
-    return !availableWeekDays.some(
-      (availableWeekDays) => availableWeekDays.week_day === weekDay,
-    )
-  })
-
-  const blockedDatesRaw = await prisma.$queryRaw`
-  SELECT * 
-  FROM schedulings S
-
-  WHERE S.user_id = ${user.id}
-    AND DATE_FORMAT(S.date, "%Y-%m") = ${`${year}-${month}`}
-  `
-
-  return res.json({ blockedWeekDays, blockedDatesRaw })
 }
